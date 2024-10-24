e3ea65e5408bea246f068dd36c76ac8dd49160b6
Dede Juniar Putra <dedejuniarputra99@gmail.com>
2024-10-25 06:03:23 +0700

    Perubahan pada modul 8

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -38,6 +28,3 @@ Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit'
 Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 
 Route::get('/show{id}', [UserController::class, 'show'])->name('user.show');
-
-
-
